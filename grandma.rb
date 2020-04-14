ae536e8e9b03066c8ac9d53666f93a37d3a3f2f4
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-14 21:04:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,11 @@
 def speak_to_grandma()
   if speak_to_grandma() == "I love you grandma"
     puts "HUH?! SPEAK UP, SONNY!"
+    elsif speak_to_grandma() == "I love you grandma"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma() == "HOW ARE YOU GRANDMA?"
+    puts "NO, NOT SINCE 1938!"
+    elsif speak_to_grandma() == "HOW ARE YOU GRANDMA?"
     puts "NO, NOT SINCE 1938!"
   else speak_to_grandma() == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN"
