39ca688d96979f85c88354e76d72b8f38602c8c4
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-14 21:04:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,14 +11,12 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-
-
-def speak_to_grandma("")
-  if speak_to_grandma("") == "I love you grandma"
+def speak_to_grandma()
+  if speak_to_grandma() == "I love you grandma"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma("") == "HOW ARE YOU GRANDMA?"
+  elsif speak_to_grandma() == "HOW ARE YOU GRANDMA?"
     puts "NO, NOT SINCE 1938!"
-  else speak_to_grandma("") == "I LOVE YOU GRANDMA"
+  else speak_to_grandma() == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN"
   end
 
