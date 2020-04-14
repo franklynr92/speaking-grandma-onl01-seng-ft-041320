15e6938fbc2801edc0fdfaa8578805758d5f9c81
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-14 21:27:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -24,11 +24,11 @@
 
 
 def speak_to_grandma(greeting)
-if greeting.upcase == 'I LOVE YOU GRANDMA!'
-  'I LOVE YOU TOO PUMPKIN!'
-   elsif  greeting.upcase == greeting
-     puts 'HUH?! SPEAK UP, SONNY!'
-  else  greeting.upcase == 'HOW ARE YOU GRANDMA?'
-     'NO, NOT SINCE 1938!'
+if greeting.upcase == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+   elsif  greeting.upcase == "HOW ARE YOU GRANDMA?"
+     "NO, NOT SINCE 1938!"
+  else  greeting.upcase =="Hi Nana, how are you?"
+     puts "HUH?! SPEAK UP, SONNY!" 
   end
 end
